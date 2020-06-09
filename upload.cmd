e6e7bb98948eb3b3ce37e9e38a1e3378d69326e1
LarsBrubaker <larsbrubaker@gmail.com>
2020-06-09 10:32:57 -0700

    help update script

---
+++ upload.cmd
@@ -0,0 +1,16 @@
+echo Delete the output directory
+PAUSE
+git worktree prune
+git branch -D gh-pages
+git fetch --all
+git pull upstream master
+wyam build
+ren output output_
+git worktree add output gh-pages
+echo Delete existing content (except for the .git file) in the output worktree
+PAUSE
+xcopy output_ output /s
+cd output
+git add --all
+git commit -m "Latest docs"
+rem git push
